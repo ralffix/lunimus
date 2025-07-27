4569c585bc34972df1f22471408150069ba3056e
ralffix <ralfs.f@outlook.com>
2025-07-27 21:40:09 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,3 +0,0 @@
-// supabase.js
-window.SUPABASE_URL = 'https://ojodoocwsgbprgxndtmp.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qb2N3c2dicHJneG5kdG1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MzYyNTUsImV4cCI6MjA2OTIxMjI1NX0.Yp4lHyHLWugYmAGJQPwZMZP7TT0o2hSsWzW4-VVEwyk';
